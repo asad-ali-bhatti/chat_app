b06bab53932aec0f7f1bac5726ae8aaff0019f3f
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2017-07-13 17:27:58 +0500

    Adds User and Roles

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module ChatApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
